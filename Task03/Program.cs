bb377e6caa92ff1010efbaa2530a1a1e1bfb0566
Anatoliy K <778966@mail.ru>
2022-09-26 11:42:14 +0500

    adedd task03

---
+++ Task03/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.Write("Введите любое число: ");
+int user_number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= user_number ; i++)
+{
+    Console.Write(Math.Pow (i , 3)+" "); 
+}

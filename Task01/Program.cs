52edb5f7d05bd920060c236252a4b7764e5c55f6
Anatoliy K <778966@mail.ru>
2022-09-26 09:53:39 +0500

    adedd task01

---
+++ Task01/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите пожалуйста пятизначное число: " );
+int number = int.Parse(Console.ReadLine()); 
+if (number < 0) number *=-1;
+if (number > 9999 && number < 100000)
+{
+    int x = number % 100; // % - остаток от деления на 100
+    int y = NumberOne (number, 2);
+    if (x / 10 == y % 10 && x % 10 == y / 10)
+    {
+        System.Console.WriteLine( number + " Число является полиндром");
+    }
+    else System.Console.WriteLine(number + " Число не является полиндром");
+} 
+else Console.WriteLine("Вы ввели не правельное число! По пробуйте еще раз!");
+int NumberOne(int num, int index) 
+{
+    while (num / Math.Pow(10, index) > 1)
+    {
+        num /= 10;
+    }
+    return num;
+}
